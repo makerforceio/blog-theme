fc547f5f9385c28602d8f80cda6e7070e107052f
Ambrose Chua <ambrose@chua.family>
2017-03-07 18:40:24 +0800

    Improve highlight.js

--- assets/js/syntax.js
+++ assets/js/syntax.js
@@ -1,6 +1,22 @@
-document.addEventListener("DOMContentLoaded", function loaded() {
-	var pres = document.querySelectorAll("pre, code");
-	[].forEach.call(pres, function each(pre) {
-		hljs.highlightBlock(pre);
+document.addEventListener('DOMContentLoaded', function loaded() {
+	var blocks = document.querySelectorAll('code');
+	[].forEach.call(blocks, function each(block) {
+		var languagePrefixRe = /\blang(?:uage)?-([\w-]+)\b/i;
+		var classes = block.className + ' ';
+		classes += block.parentNode ? block.parentNode.className : '';
+		var match = languagePrefixRe.exec(classes);
+
+		if (match && !hljs.getLanguage(match[1])) {
+			var head = document.head;
+			var script = document.createElement('script');
+			script.src = '/assets/js/highlight/languages/' + match[1] + '.min.js';
+			script.addEventListener('load', function canHighlight() {
+				hljs.highlightBlock(block);
+			});
+			head.appendChild(script);
+			return;
+		}
+
+		hljs.highlightBlock(block);
 	});
 });
---
+++ fetch-hljs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+VERSION=9.9.0
+LANGS="python javascript cpp arduino scss css xml json markdown http bash nginx php"
+set -e
+
+BASE=$(dirname $(realpath "$0"))
+[ ! -d $BASE/tmp/ ]
+mkdir -p $BASE/tmp/
+wget -O $BASE/tmp/highlight.js.zip https://github.com/isagalaev/highlight.js/archive/$VERSION.zip
+unzip $BASE/tmp/highlight.js -d $BASE/tmp/
+
+cd $BASE/tmp/highlight.js-*/
+npm install
+node ./tools/build.js -t cdn $LANGS
+echo "Copying ./build/* to $BASE/assets/js/highlight/"
+rm -r $BASE/assets/js/highlight/ || true
+mkdir -p $BASE/assets/js/highlight/
+cp -R ./build/* $BASE/assets/js/highlight/
+cd $BASE/
+
+rm -r $BASE/tmp/
