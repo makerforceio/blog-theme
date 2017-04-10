3f1f02b11ee2d4441e4328aed80a47a3d6a40f86
Ambrose Chua <ambrose@makerforce.io>
2017-04-10 14:04:01 +0800

    Update syntax.js

--- assets/js/syntax.js
+++ assets/js/syntax.js
@@ -1,30 +1,30 @@
-document.addEventListener('DOMContentLoaded', function loaded() {
+(function () {
     var i = setInterval(function testLoaded() {
-        if (hljs) {
+        if (hljs && document.readyState === "complete") {
             clearInterval(i);
             highlight();
         }
     }, 10);
-});
-function highlight() {
-	var blocks = document.querySelectorAll('code');
-	[].forEach.call(blocks, function each(block) {
-		var languagePrefixRe = /\blang(?:uage)?-([\w-]+)\b/i;
-		var classes = block.className + ' ';
-		classes += block.parentNode ? block.parentNode.className : '';
-		var match = languagePrefixRe.exec(classes);
-
-		if (match && !hljs.getLanguage(match[1])) {
-			var head = document.head;
-			var script = document.createElement('script');
-			script.src = '/assets/js/highlight/languages/' + match[1] + '.min.js';
-			script.addEventListener('load', function canHighlight() {
-				hljs.highlightBlock(block);
-			});
-			head.appendChild(script);
-			return;
-		}
-
-		hljs.highlightBlock(block);
-	});
-}
+    function highlight() {
+    	var blocks = document.querySelectorAll('code');
+    	[].forEach.call(blocks, function each(block) {
+    		var languagePrefixRe = /\blang(?:uage)?-([\w-]+)\b/i;
+    		var classes = block.className + ' ';
+    		classes += block.parentNode ? block.parentNode.className : '';
+    		var match = languagePrefixRe.exec(classes);
+    
+    		if (match && !hljs.getLanguage(match[1])) {
+    			var head = document.head;
+    			var script = document.createElement('script');
+    			script.src = '/assets/js/highlight/languages/' + match[1] + '.min.js';
+    			script.addEventListener('load', function canHighlight() {
+    				hljs.highlightBlock(block);
+    			});
+    			head.appendChild(script);
+    			return;
+    		}
+    
+    		hljs.highlightBlock(block);
+    	});
+    }
+})();
