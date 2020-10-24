5f7bec90a27a20e956a1fb01d64152b22b7862cf
Ambrose Chua <ambrose@makerforce.io>
2020-10-24 12:48:10 +0800

    Experiment with new Koneig styles

--- assets/js/syntax.js
+++ assets/js/syntax.js
@@ -1,21 +1,19 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-	document.querySelectorAll('pre code').forEach((block) => {
-		// Detect language and check if loaded
-		const languageDetectRe = /\blang(?:uage)?-([\w-]+)\b/i;
-		const match = languageDetectRe.exec(block.className);
+document.querySelectorAll('pre code').forEach((block) => {
+	// Detect language and check if loaded
+	const languageDetectRe = /\blang(?:uage)?-([\w-]+)\b/i;
+	const match = languageDetectRe.exec(block.className);
 
-		if (match && !hljs.getLanguage(match[1])) {
-			// Load language definition
-			var script = document.createElement('script');
-			script.src = '/assets/js/highlight/languages/' + match[1] + '.min.js';
-			script.addEventListener('load', () => {
-				hljs.highlightBlock(block);
-			});
-			document.head.appendChild(script);
-			return;
-		}
+	if (match && !hljs.getLanguage(match[1])) {
+		// Load language definition
+		var script = document.createElement('script');
+		script.src = '/assets/js/highlight/languages/' + match[1] + '.min.js';
+		script.addEventListener('load', () => {
+			hljs.highlightBlock(block);
+		});
+		document.head.appendChild(script);
+		return;
+	}
 
-		// Highlight immediately
-		hljs.highlightBlock(block);
-	});
+	// Highlight immediately
+	hljs.highlightBlock(block);
 });
