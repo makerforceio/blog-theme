af345bb008cc2dace0c1d9a7c5e4d4f1cd91e06b
Ambrose Chua <ambrose@hey.com>
2021-05-28 00:17:24 +0800

    Add special wide-code tag

--- fetch-katex.sh
+++ fetch-katex.sh
@@ -9,6 +9,9 @@ mkdir -p tmp/
 wget -O tmp/katex.zip https://github.com/KaTeX/KaTeX/releases/download/v$VERSION/katex.zip
 unzip tmp/katex.zip -d tmp/
 
+echo "Copying katex to $TARGET/"
+rm -r $TARGET/ || true
+mkdir -p $TARGET/
 mv tmp/katex $TARGET
 
 rm -r tmp/
